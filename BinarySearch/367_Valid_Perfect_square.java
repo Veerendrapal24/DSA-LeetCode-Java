1206c261a40b1d03e10c5912c2313a7716628e8b
Veerendrapal24 <veerendra2475@gmail.com>
2026-06-23 14:42:21 +0530

    Binary Search

---
+++ BinarySearch/367_Valid_Perfect_square.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long high =num;
+        long low =0;
+        while(low<=high){
+            long mid = low+(high-low)/2;
+            if(mid*mid==num){
+                return true;
+            }
+            else if(mid*mid>=num){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return false;
+    }
+}
