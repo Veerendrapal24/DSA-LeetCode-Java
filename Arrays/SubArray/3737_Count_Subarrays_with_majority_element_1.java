67dcedd2c62e5e2e4912c256b6d2f7606601e539
Veerendrapal24 <veerendra2475@gmail.com>
2026-06-25 10:51:15 +0530

    subarray

---
+++ Arrays/SubArray/3737_Count_Subarrays_with_majority_element_1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countMajoritySubarrays(int[] nums, int target) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            int length=0;
+            int s=0;
+            for(int j=i;j<nums.length;j++){
+               length++;
+               if(nums[j]==target) s++;
+               if(s>length/2) count++;
+            }
+            
+        }
+        return count;
+    }
+}
